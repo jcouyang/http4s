0b448a1adf4ad3832a5d0421e9b3705ca64f071e
Jichao Ouyang <oyanglulu@gmail.com>
2019-05-07 14:27:14 +1000

    covert write body into finagle request

--- finagle-client/src/main/scala/org/http4s/client/finagle/Finagle.scala
+++ finagle-client/src/main/scala/org/http4s/client/finagle/Finagle.scala
@@ -5,17 +5,20 @@ package finagle
 import cats.effect._
 import cats.syntax.functor._
 import com.twitter.finagle.{Http,Service}
-import com.twitter.finagle.http.{Request => Req, Response=>Resp, Version, Method, RequestBuilder}
+import com.twitter.finagle.http.{Request => Req, Response=>Resp, Method, RequestBuilder}
 import com.twitter.util.{Return, Throw, Future}
-import java.io.PipedInputStream
-import fs2.io._
+import cats.syntax.flatMap._
+import com.twitter.io.Buf
 
 object Finagle {
 
   def allocate[F[_]](svc: Service[Req, Resp])(
       implicit F: ConcurrentEffect[F]): F[Client[F]] =
        F.delay(Client[F] { req =>
-         Resource.liftF(toF(svc(toFinagleReq(req))).map(toHttp4sResp))
+         Resource.liftF(for{
+           freq <- toFinagleReq(req)
+           resp <- toF(svc(freq))
+         }yield resp).map(toHttp4sResp)
         })
 
   def resource[F[_]](dest: String)(
@@ -23,15 +26,17 @@ object Finagle {
     Resource.make(F.delay(Http.newService(dest))){svc => toF(svc.close())}
     .flatMap(svc => Resource.liftF(allocate(svc)))
   }
-  def toFinagleReq[F[_]](req: Request[F]):F[Req] = {
-    val httpVersion = req.httpVersion match {
-      case HttpVersion.`HTTP/1.0` => Version.Http10
-      case _ => Version.Http11
-    }
+  def toFinagleReq[F[_]](req: Request[F])(implicit F: ConcurrentEffect[F]):F[Req] = {
     val method = Method(req.method.name)
-    val stream = new PipedInputStream()
-    val finagleReq = RequestBuilder().url(req.uri).addHeaders(req.headers)
-    req.body.through(toInputStream).compile.toList.headOption.map(finagleReq.build(method, _))
+    val finagleReq = RequestBuilder()
+      .url(req.uri.toString())
+      .addHeaders(req.headers.toList.map(h => (h.name.toString, h.value)).toMap)
+      .build(method,Some(Buf.Empty))
+    req.body.mapChunks{bytes =>
+      finagleReq.write(bytes.toArray)
+      bytes
+    }
+      .compile.drain.map(_ => finagleReq)
   }
 
   def toHttp4sResp[F[_]](resp: Resp): Response[F] = ???
